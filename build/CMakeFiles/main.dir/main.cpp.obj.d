db16724bbeb3558cbebfa9293d5d8b495a3416c7
while <wuzongyang2006@163.com>
2024-09-27 20:07:06 +0800

    initial commit

---
+++ build/CMakeFiles/main.dir/main.cpp.obj.d
@@ -0,0 +1,19 @@
+CMakeFiles/main.dir/main.cpp.obj: D:\VSC\homework\main.cpp \
+ D:/MinGW/mingw64/lib/gcc/x86_64-w64-mingw32/8.1.0/include/c++/cstdio \
+ D:/MinGW/mingw64/lib/gcc/x86_64-w64-mingw32/8.1.0/include/c++/x86_64-w64-mingw32/bits/c++config.h \
+ D:/MinGW/mingw64/lib/gcc/x86_64-w64-mingw32/8.1.0/include/c++/x86_64-w64-mingw32/bits/os_defines.h \
+ D:/MinGW/mingw64/lib/gcc/x86_64-w64-mingw32/8.1.0/include/c++/x86_64-w64-mingw32/bits/cpu_defines.h \
+ D:/MinGW/mingw64/x86_64-w64-mingw32/include/stdio.h \
+ D:/MinGW/mingw64/x86_64-w64-mingw32/include/crtdefs.h \
+ D:/MinGW/mingw64/x86_64-w64-mingw32/include/_mingw.h \
+ D:/MinGW/mingw64/x86_64-w64-mingw32/include/_mingw_mac.h \
+ D:/MinGW/mingw64/x86_64-w64-mingw32/include/_mingw_secapi.h \
+ D:/MinGW/mingw64/x86_64-w64-mingw32/include/vadefs.h \
+ D:/MinGW/mingw64/x86_64-w64-mingw32/include/sdks/_mingw_directx.h \
+ D:/MinGW/mingw64/x86_64-w64-mingw32/include/sdks/_mingw_ddk.h \
+ D:/MinGW/mingw64/x86_64-w64-mingw32/include/_mingw_print_push.h \
+ D:/MinGW/mingw64/x86_64-w64-mingw32/include/_mingw_off_t.h \
+ D:/MinGW/mingw64/x86_64-w64-mingw32/include/swprintf.inl \
+ D:/MinGW/mingw64/x86_64-w64-mingw32/include/sec_api/stdio_s.h \
+ D:/MinGW/mingw64/x86_64-w64-mingw32/include/_mingw_print_pop.h \
+ D:\VSC\homework\a.h
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include<cstdio>
+#include"a.h"
+int main()
+{
+    printf("Hello World\n");
+    print10();
+    return 0;
+}
